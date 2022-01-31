70e0aab38edc6299cbd89f73964e19a52a51800d
Francois ROLAND <francois.roland@iepscf-uccle.be>
2022-01-31 10:51:32 +0000

    Clean up.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,5 @@
 import React, { useEffect, useReducer, useState } from "react";
 
-const initialStories = [
-  {
-    title: "React",
-    url: "https://reactjs.org",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () =>
-  new Promise((resolve, reject) =>
-    setTimeout(() => {
-      resolve({ data: { stories: initialStories } });
-      // reject();
-    }, 2000)
-  );
-
 const useSemiPersistentState = (key, initialValue) => {
   const [value, setValue] = useState(localStorage.getItem(key) || initialValue);
 
